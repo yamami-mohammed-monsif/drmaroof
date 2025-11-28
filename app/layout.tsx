b801ff9e6afa4d0bbf0d776ad71dafd6bf35d874
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-11-28 00:29:47 +0000

    added GTM code snippets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Gilda_Display, Source_Sans_3 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const gildadisplay = Gilda_Display({
@@ -22,7 +23,6 @@ const sourcesans3 = Source_Sans_3({
   adjustFontFallback: true,
 });
 
-// Metadata export now only contains SEO/Content-related tags
 export const metadata: Metadata = {
   title: "Transform Your Smile | Dr. Maroof Beg Dental Visagism",
   description:
@@ -32,7 +32,6 @@ export const metadata: Metadata = {
   authors: [{ name: "Dr. Maroof Beg" }],
 };
 
-// Viewport export contains device/browser configuration tags
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
@@ -46,9 +45,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PT9HRTXJ');
+          `}
+        </Script>
+      </head>
       <body
         className={`${gildadisplay.variable} ${sourcesans3.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PT9HRTXJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
       </body>
     </html>
